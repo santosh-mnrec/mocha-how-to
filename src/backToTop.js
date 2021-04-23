6a9295d61c8b1eb5bb0f14594d7943bf221b5a2d
Santosh Singh <Santosh_Singh2@Dell.com>
2021-04-23 15:46:47 +0800

    R

--- src/backToTop.js
+++ src/backToTop.js
@@ -1,21 +1,16 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM(`...`);
-var $ = require("jquery")(window);
-
+const $=require("./Jquery");
 const BackToTop = (function () {
-  var initBackToTop = () => {
-    BackToTop.animate();
-    $(".sd-back-to-top").on("click.sdbacktotop",BackToTop.animate);
+  var baclTop = () => {
+    $(".back").on("click", animate);
   };
   var animate = function () {
     $("html, body").animate({ scrollTop: "0" }, 1000);
   };
 
   return {
-    initBackToTop,
+    baclTop,
     animate,
   };
 })();
-BackToTop.initBackToTop();
+
 module.exports = BackToTop;
--- test/backToTop.spec.js
+++ test/backToTop.spec.js
@@ -1,22 +1,37 @@
-const BackToTop = require("../src/backToTop");
+const jsdom = require("jsdom");
 const sinon = require("sinon");
-const jsdom = require('jsdom')
-const document = new jsdom.JSDOM('<html></html>', {})
-const window = document.defaultView
-global.document = document
-global.window = window
-
-const { expect } = require("chai");
+const expect = require("chai").expect;
+const proxyquire = require("proxyquire");
 
 describe("Name of the group", () => {
-  it("should ", () => {
-    const operation = sinon.spy(BackToTop,"animate");
-    
-    BackToTop.initBackToTop();
+  before(() => {
+    const html =
+      '<!doctype html><html><head><meta charset="utf-8">' +
+      "</head><body><div class='back'></div></body></html>";
 
-    $(".sd-back-to-top").on('click',operation);
-    expect(operation.called);
-    console.log(operation.callCount);
- 
+    const document = new jsdom.JSDOM(html);
+    const window = document.window;
+
+    require("jquery")(window);
+    global.document = window.document;
+    global.window = window;
+  });
+  afterEach(() => {
+    sinon.restore();
+  });
+  it("should ", (done) => {
+    // const onStub = sinon.stub().callsFake((handler) => handler());
+    // const jqueryStub = { on: onStub };
+    // const $ = sinon.stub().callsFake(() => jqueryStub);
+
+    // $.withArgs(".back").returns(onStub);
+    // const BackToTop = proxyquire("../src/backToTop", { jQuery: $ });
+
+    const b=sinon.mock("../src/backToTop");
+    
+    b.expects('saveRemainLoggedInPreference').once().withArgs(true);
+    //expect(onStub.callCount).to.be.eq(1);
+
+    done();
   });
 });
